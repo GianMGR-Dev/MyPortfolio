f738e1c0fc680d78c2a87e88429262e130e3802e
GianMGR-Dev <giangutierrez.dev@gmail.com>
2025-12-07 17:40:55 -0400

    Changes on Social Network icons

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -1,21 +1,28 @@
 import Link from 'next/link';
+import { Linkedin, Github} from 'lucide-react';
 
 const SocialNetworks = () => {
+
+      const socialLinks = [
+        { name: 'GitHub', icon: Github, href: 'https://github.com/GianMGR-Dev', color: 'hover:text-[var(--white)]' },
+        { name: 'LinkedIn', icon: Linkedin, href: 'https://www.linkedin.com/in/gian-gutierrez-7a859737b/', color: 'hover:text-blue-400' }
+      ];
+
     return (
         <div className="p-2 rounded-3xl flex flex-row justify-center items-center gap-4">
-
-            <a className="bg-white p-2 rounded-full transition-transform duration-100 ease-in-out hover:scale-115" href='https://www.linkedin.com/in/gian-gutierrez-7a859737b/?locale=en_US' target="_blank" rel="noopener noreferrer">
-                <svg width="30" height="30">
-                    <use href="/sprite.svg#linkedin" />
-                </svg>
-            </a>
-            
-            <a className="bg-gray-950 p-2 rounded-full transition-transform duration-100 ease-in-out hover:scale-115" href='https://github.com/GianMGR-Dev' target="_blank" rel="noopener noreferrer">
-                <svg width="30" height="30">
-                    <use href="/sprite.svg#github" />
-                </svg>
-            </a>
-
+            <div className="flex space-x-4">
+            {socialLinks.map((link) => (
+                <a 
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={link.name}
+                className={`transition duration-300 ease-in-out text-gray-500 ${link.color}`}
+                >
+                <link.icon size={32} />
+                </a>
+            ))}
+            </div>
         </div>
     )
 }
